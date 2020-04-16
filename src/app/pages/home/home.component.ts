42785d69c3b4d7de0b21d3eb013b27a075981892
sarfophil <philsarfogh@gmail.com>
2020-04-15 21:06:04 -0500

    Fix Socket Notification

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PostType } from 'src/app/model/post-type';
 import {SocketioService} from "../../service/socket/socketio.service";
 import {User} from "../../model/user";
+import {API_TYPE} from "../../model/apiType";
+import {NotificationResponse} from "../../model/notification-response";
+import {ProviderService} from "../../service/provider-service/provider.service";
 
 @Component({
   selector: 'app-home',
@@ -11,13 +14,20 @@ import {User} from "../../model/user";
 export class HomeComponent implements OnInit {
   postState :PostType = PostType.HOMEPAGE_POSTS;
   user: User;
-  constructor(private socketioService: SocketioService) { }
+  constructor(private socketioService: SocketioService,private provider: ProviderService) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('active_user'))
     this.socketioService.onNotificationReceivedEvent(this.user.email)
+    this.checkNotification()
   }
 
+
+  checkNotification(){
+    let path = `check_notification`
+    this.provider.get(API_TYPE.USER, path,'')
+      .subscribe((res) => {})
+  }
 
 
 }
